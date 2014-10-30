39861805af6554128e16f57191ba001d861bda67
Greg Arndt <garndt@mozilla.com>
2014-10-30 16:07:42 -0500

    Added linux64-mulet build task

---
+++ testing/docker/builder/build-firefox.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -live
+
+################################### build_firefox.sh ###################################
+
+. build-setup.sh
+
+### Check that require variables are defined
+test $REPOSITORY  # Should be an hg repository url to pull from
+test $REVISION    # Should be an hg revision to pull down
+test $MOZCONFIG   # Should be a mozconfig file from mozconfig/ folder
+
+
+### Pull and update mozilla-central
+cd $gecko_dir
+hg pull -r $REVISION $REPOSITORY;
+hg update $REVISION;
+
+./mach build;
+
+### Make package
+cd /home/worker/object-folder;
+make package package-tests;
+
+### Extract artifacts
+# Navigate to dist/ folder
+cd /home/worker/object-folder/dist;
+
+ls -lah /home/worker/object-folder/dist/
+
+
+# Target names are cached so make sure we discard them first if found.
+rm -f target.linux-x86_64.tar.bz2 target.linux-x86_64.json target.tests.zip
+
+# Artifacts folder is outside of the cache.
+mkdir -p /home/worker/artifacts/
+
+# Discard version numbers from packaged files, they just make it hard to write
+# the right filename in the task payload where artifacts are declared
+mv *.linux-x86_64.tar.bz2   /home/worker/artifacts/target.linux-x86_64.tar.bz2
+mv *.linux-x86_64.json      /home/worker/artifacts/target.linux-x86_64.json
+mv *.tests.zip              /home/worker/artifacts/target.tests.zip
+
+################################### build.sh ###################################
