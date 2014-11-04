7f37b80f593f336517fa44403cce6a7028c8f536
Greg Arndt <garndt@mozilla.com>
2014-11-04 12:04:02 -0600

    User root user for now for builds until volume mount permissions are resolved

--- testing/docker/builder/run-as-worker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Take care of any volume mounts created by a user other than worker
-chown -R worker:worker /home/worker/
-
-sudo -E -u worker "$@"
--- testing/docker/builder/system-setup.sh
+++ testing/docker/builder/system-setup.sh
@@ -6,9 +6,11 @@ home="/home/worker"
 
 mkdir -p $home/bin
 mkdir -p $home/tools
-mkdir -p $home/tools/tooltool_cache
+mkdir -p $home/tools/tooltool-cache
 
 wget -O $home/tools/tooltool.py https://raw.githubusercontent.com/mozilla/build-tooltool/master/tooltool.py
+
+chown -R worker:worker /home/worker/* /home/worker/.*
 
 # Remove the setup.sh setup, we don't really need this script anymore, deleting
 # it keeps the image as clean as possible.
